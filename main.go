1f9f06065045fbf3da622a45a4396bfa0ecae65b
Jaden Hwang <hwangjaden@gmail.com>
2026-08-12 00:06:53 -0400

    Intial commit: gitingnore, go module, and main code file

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+	"flag"
+	"strings"
+
+	"https://github.com/J-Hwang7/raft-kv.git"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fmt.Println("goroutine says hi")
-	}()
-	wg.Wait()
-	fmt.Println("raft-kv toolchain works")
+	id := flag.Int("id", 0, "this node's 0-indexed ID")
+	peerList := flag.String(
+		"peers",
+		"localhost:8000,localhost:8001,localhost:8002",
+		"comma-separated addresses of all nodes",
+	)
+	httpAddr := flag.String("http", "", "HTTP API address (e.g. localhost:9000)")
+	flag.Parse()
+
+	peers := strings.Split(*peerList, ",")
+
+	node := raft.NewRaftNode(*id, peers)
+	if err := node.StartServer(); err != nil {
+		panic(err)
+	}
+
+	if *httpAddr != "" {
+		node.StartHTTP(*httpAddr)
+	}
+
+	go node.Run()
+	select {}
 }
